ec2c249d52c55b365c6ec6bbc1312fd5e506480f
Saviskar <Saviskar>
2025-10-10 08:11:33 +0530

    feat: added gender dropdown to create account

--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -25,6 +25,47 @@ class CreateAccount extends StatelessWidget {
             InputField(hintText: 'Mobile Number'),
             InputField(hintText: 'Delivery Address'),
             InputField(hintText: 'Password'),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                hintText: 'Gender',
+                filled: true,
+                fillColor: AppColors.background, // same as your input field bg
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 16,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(color: Colors.black),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                    color: AppColors.primary,
+                    width: 2,
+                  ),
+                ),
+              ),
+              dropdownColor: AppColors.background,
+              icon: const Icon(
+                Icons.arrow_drop_down_rounded,
+                color: AppColors.primary,
+              ),
+              items: const [
+                DropdownMenuItem(value: "Male", child: Text('Male')),
+                DropdownMenuItem(value: "Female", child: Text('Female')),
+                DropdownMenuItem(value: "Other", child: Text('Other')),
+              ],
+              onChanged:
+                  (
+                    value,
+                  ) {}, // keep empty or null if you don’t want it to do anything
+            ),
+            const SizedBox(height: 20),
             TextButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
